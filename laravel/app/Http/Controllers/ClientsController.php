65cc11a62c7894b75d498fc88822b5d6db3bfad7
Florian VIDAL <florianvidal@icloud.com>
2016-06-07 21:22:09 +0200

    Création des Controller Clients et Commandes, développement des fonctions CRUD

--- laravel/app/Http/Controllers/ClientsController.php
+++ laravel/app/Http/Controllers/ClientsController.php
@@ -2,11 +2,76 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
+use DB;
+use Request;
+use App\Http\Requests\EditClientRequest;
+use App\Prestations;
+use App\Commandes;
+use App\Clients;
+use App\Employes;
+use App\Produits;
+use Auth;
+use DateTime;
+use Carbon\Carbon;
 use App\Http\Requests;
-
-class ClientsController extends Controller
+//
+class ClientController extends Controller
 {
+    public function ClientEdit (){
+    	 $userId = Auth::id();
+    	 $NomClient = '';
+    	 $PrenomClient = '';
+    	 $AdresseClient = '';
+    	 $CodePostalClient = '';
+    	 $VilleClient = '';
+    	 $emailClient = '';
+    	 $telClient = '';
+    	 $passClient = '';
+    	 $leClient = Clients::where('id', $userId)
+               ->take(1)
+               ->get();
+     	 //
+     	 foreach ($leClient as $leClient) {
+     	 	$NomClient = $leClient->nom;
+     	 	$PrenomClient = $leClient->prenom;
+     	 	$AdresseClient = $leClient->adresse;
+     	 	$CodePostalClient = $leClient->codepostal;
+     	 	$VilleClient = $leClient->ville;
+     	 	$emailClient = $leClient->email;
+     	 	$telClient = $leClient->tel;
+     	 	$passClient = $leClient->password;
+     	 }
+     	 //
+      return view('prestationsviews/editclient', compact('leClient','NomClient','PrenomClient','AdresseClient','CodePostalClient','VilleClient','emailClient','telClient','passClient'))
+                       ->with('leClient', $leClient)
+                       ->with('NomClient', $NomClient)
+                       ->with('PrenomClient', $PrenomClient)
+                       ->with('AdresseClient', $AdresseClient)
+                       ->with('CodePostalClient', $CodePostalClient)
+                       ->with('VilleClient', $VilleClient)
+                       ->with('emailClient', $emailClient)
+                       ->with('telClient', $telClient)
+                       ->with('passClient', $passClient);
+        //
+    }
     //
+    public function store(){
+    // Sauvegarde des nouvelles informations du client dans la BDD, on recupère l'utilisateur co avec Auth:: ,
+    // on recupère toutes les informatiosn du form avec le Request::all() et ensuite on met tout dans un array/variable update
+    // avec un for each on parcours l'array que la Request nous à renvoyé et on le file avec une requete update sur la BDD pour update les champs.
+    $userId = Auth::id();
+    $data = Request::all();
+		$update = [
+		            'nom'     => $data['nom'],
+		            'prenom'   => $data['prenom'],
+		            'adresse'     => $data['adresse'],
+		            'codepostal'    => $data['codepostal'],
+		            'ville'    => $data['ville'],
+		            'email'    => $data['email'],
+		            'tel'    => $data['tel'],
+		            'password'    => bcrypt($data['password']),
+		        ];
+    	DB::table('Clients')->where('id', $userId)->limit(1)->update($update);
+    	return view('prestationsviews/dashboard');
+	}
 }
--- laravel/app/Http/Controllers/CommandesController.php
+++ laravel/app/Http/Controllers/CommandesController.php
@@ -1,12 +1,112 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
+use DB;
+use Request;
+use App\Http\Requests\CreateReservationRequest;
+use App\Prestations;
+use App\Commandes;
+use App\Clients;
+use App\Employes;
+use App\Produits;
+use Auth;
+use DateTime;
+use Carbon\Carbon;
 use App\Http\Requests;
 
 class CommandesController extends Controller
 {
-    //
+    public function create()
+    {
+
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+      $data = Request::all();
+      $commande = Commandes::where('clients_id', $data['clients_id'])
+               ->where('Commandes.dateDepot', $data['dateDepot'])
+               ->take(1)
+               ->get();
+      $client = Clients::where('id', $data['clients_id'])
+               ->take(1)
+               ->get();
+      $produit = Produits::where('id', $data['produits_id'])
+               ->take(1)
+               ->get();
+      $prestation = Prestations::where('id', $data['prestations_id'])
+               ->take(1)
+               ->get();
+      return view('prestationsviews/confirmationReservation', compact('commande','client','produit','prestation'))
+                       ->with('commande', $commande)
+                       ->with('client', $client)
+                       ->with('produit', $produit)
+                       ->with('prestation', $prestation);
+    }
+    //------------------------------------------------------------------------------------------//
+    public function priseReservation()
+    {
+       $userId = Auth::id();
+       $produits = DB::table('Produits')->lists('nom', 'id');
+       $prestations = DB::table('Prestations')->lists('nom', 'id');
+       return view('prestationsviews/reservation', compact('userId','produits','prestations'))
+                       ->with('userId', $userId)
+                       ->with('prestations', $prestations)
+                       ->with('produits', $produits);
+    }
+    //------------------------------------------------------------------------------------------//
+	 /**
+	 * Fonction de récupération des commandes, jointure sur les tables clients, produits, reservations, employes.
+	 * on retourne un objet avec toutes les infos relatives au prestations du client.
+	 */
+    //------------------------------------------------------------------------------------------//
+    public function RecupCommandes()
+    {
+        $userId = Auth::id();
+        // Requête pour récupérer les prestations du clients
+    		$Commandes = DB::table('Commandes')->where('Commandes.clients_id', $userId)
+    			      ->join('Clients', 'clients_id', '=', 'Clients.id')
+                ->join('Prestations', 'prestations_id', '=', 'Prestations.id')
+                ->join('Employes', 'employes_id', '=', 'Employes.id')
+                ->join('Produits', 'produits_id', '=', 'Produits.id')
+                ->selectRaw('Employes.nom as nomEmploye, Employes.prenom as prenomEmploye, Employes_id as idEmploye, Clients.nom as nomClient, Clients.prenom as prenomClient, commentaire, dateDepot, pretPourRecuperation, dateRecuperation, adresse, codepostal, ville, email, tel, dateArrivee, typeContrat, salaire, Prestations.nom as nomPrestation, Produits.nom as nomProduit')
+                ->orderBy('Commandes.id','desc')
+                ->get();
+        //------------------------------------------------------------------------------------------//
+        $CalendrierFetch = json_encode(DB::table('Commandes')->where('Commandes.clients_id', $userId)
+                ->join('Clients', 'clients_id', '=', 'Clients.id')
+                ->join('Prestations', 'prestations_id', '=', 'Prestations.id')
+                ->join('Produits', 'produits_id', '=', 'Produits.id')
+                ->orderBy('dateDepot')
+                ->lists('dateDepot','Produits.nom'));
+        $GraphCommandesT = DB::table('Commandes')->where('Commandes.clients_id', $userId)
+                ->join('Clients', 'clients_id', '=', 'Clients.id')
+                ->join('Prestations', 'prestations_id', '=', 'Prestations.id')
+                ->join('Produits', 'produits_id', '=', 'Produits.id')
+                ->join('Tarifs', 'Tarifs.Prestations_id', '=', 'Prestations.id')
+                ->selectRaw('Produits.nom, COUNT(*) as count')
+                ->groupBy('Produits.nom')
+                ->orderBy('count', 'desc')
+                ->lists('Produits.nom');
+        //------------------------------------------------------------------------------------------//
+        $GraphCommandesN = DB::table('Commandes')->where('Commandes.clients_id', $userId)
+                ->join('Clients', 'clients_id', '=', 'Clients.id')
+                ->join('Prestations', 'prestations_id', '=', 'Prestations.id')
+                ->join('Produits', 'produits_id', '=', 'Produits.id')
+                ->join('Tarifs', 'Tarifs.Prestations_id', '=', 'Prestations.id')
+                ->selectRaw('Produits.nom, COUNT(*) as count')
+                ->groupBy('Produits.nom')
+                ->orderBy('count', 'desc')
+                ->lists('count');
+        // Requête séparé en deux parties pour les statistiques client avec ChartsJS
+        return view('prestationsviews/dashboard', compact('GraphCommandesT','GraphCommandesN','Commandes'))
+                       ->with('Commandes', $Commandes)
+                       ->with('GraphCommandesT', $GraphCommandesT)
+                       ->with('GraphCommandesN', $GraphCommandesN);
+        //------------------------------------------------------------------------------------------//
+    }
 }
