cca1a6b681d710f12557f6193b87876654501c8c
Florian VIDAL <florianvidal@icloud.com>
2016-06-07 21:01:50 +0200

    Création du CSS de la homepage et des pages de login, début de création des views

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -4,17 +4,95 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pressing des Halles</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.3/Chart.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.3/Chart.bundle.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-scrollTo/2.1.0/jquery.scrollTo.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.3/Chart.bundle.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.3/Chart.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/animatecss/3.5.1/animate.css" integrity="sha256-DbEot+lC/Kpjr33eXzHSzQQZNrDS9IYQRXxj/KvBrJc=" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700,300italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+        <style>
+              main {
+                flex: 1 0 auto;
+              };
 
-    <title>Laravel</title>
+        .btn-large {
+            height: 35px;
+            line-height: 35px;
+            }
 
+        select {
+            display: block!important;
+        }
+        .btn, .btn-large {
+            text-decoration: none;
+            color: #fff;
+            background-color: #26a69a;
+            text-align: center;
+            letter-spacing: .5px;
+            transition: .2s ease-out;
+            cursor: pointer;
+            padding-bottom: 5px;
+            height: 40px;
+            line-height: 40px;
+            background-color: #363C44;
+            color: white;
+            font-weight: 400;
+            font-size: 11px;
+            border-left: 2px;
+            border-right: 2px;
+            border-bottom: 2px;
+            border-top: 2px;
+            border-style: solid;
+        };
+        .btn:hover, .btn-large:hover {
+            text-decoration: none;
+            color: #363C44;
+            background-color: #fff;
+            text-align: center;
+            letter-spacing: .5px;
+            transition: .2s ease-out;
+            cursor: pointer;
+            padding-bottom: 5px;
+            line-height: 40px;
+            font-weight: 400;
+            font-size: 11px;
+            border-left: 2px;
+            border-right: 2px;
+            border-bottom: 2px;
+            border-top: 2px;
+            border-style: solid;
+            /* height: 20px; */
+        };
+        </style>
+        <script>
+        $(document).ready(function() {
+            $('.js-scrollTo').on('click', function() {
+                var page = $(this).attr('href');
+                var speed = 750;
+                $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+                return false;
+            });
+        });
+    </script>
     <!-- Fonts -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
-
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
     <!-- Styles -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
-
     <style>
         body {
             font-family: 'Lato';
@@ -25,58 +103,48 @@
         }
     </style>
 </head>
-<body id="app-layout">
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
+<body id="app-layout" style="background-color: rgba(255, 87, 34, 0.88);">
+<!-- NAVIGATION EN HAUT -->
+<nav>
+<!-- Navigation avec un if pour voir si l'utilisateur est authentifié du coup on change le menu en fonction -->
+    @if (Auth::guest())
+    <div class="nav-wrapper" style="border-bottom: 0px;">
+    @else
+    <div class="nav-wrapper">
+    @endif
+    <a class="brand-logo" href="{{ url('/dashboard') }}">
+    @if (Auth::guest())
+    <img src="/img/Logo.png" style="width:30px;">
+    <p style="color:#363C44;display: inline-block;padding: 0;margin-top: -10px;font-weight: 400;margin-top: -50px;">Pressing des Halles</p>
+    @else
+    <p style="color:#363C44;display: inline-block;padding: 0;margin-top: -10px;font-weight: 400;font-style: italic;"> Mon</p> pressing : <p style="color:#363C44;display: inline-block;padding: 0;margin-top: -10px;font-weight: 400;font-style: italic;"> mon</p> espace client</a>
+    @endif
+    </a>
+    <ul id="nav-mobile" class="right hide-on-med-and-down">
+        @if (Auth::guest())
+            <li><a class="js-scrollTo" href="#infos" >A propos du pressing</a></li>
+            <li><a class="js-scrollTo" href="#tarifs" >Tarifs</a></li>
+            <li><a class="waves-effect waves-light btn-large" style="padding-bottom: 5px;height: 40px;line-height: 40px;background-color: white;color: #363C44;font-weight: 400;font-size: 11px;border-left: 2px;border-right: 2px;border-bottom: 2px;border-top: 2px;border-style: solid;" href="{{ url('/login') }}">Connexion</a></li>
+            <li><a class="waves-effect waves-light btn-large" style="padding-bottom: 5px;height: 40px;line-height: 40px;background-color: white;color: #363C44;font-weight: 400;font-size: 11px;border-left: 2px;border-right: 2px;border-bottom: 2px;border-top: 2px;border-style: solid;" href="{{ url('/register') }}">Enregistrement</a></li>
+        @else
+            <li><a class="waves-effect waves-light btn-large" href="{{ url('/dashboard') }}" style="padding-bottom: 5px;height: 40px;line-height: 40px;background-color: #363C44;color: white;font-weight: 400;font-size: 11px;border-left: 2px;border-right: 2px;border-bottom: 2px;border-top: 2px;border-style: solid;" >Mon historique</a></li>
+            <li><a class="waves-effect waves-light btn-large" href="{{ url('/reservation') }}" style="padding-bottom: 5px;height: 40px;line-height: 40px;background-color: #363C44;color: white;font-weight: 400;font-size: 11px;border-left: 2px;border-right: 2px;border-bottom: 2px;border-top: 2px;border-style: solid;">Faire une réservation</a></li>
+            <li><a class="waves-effect waves-light btn-large" href="{{ url('/compte') }}" style="padding-bottom: 5px;height: 40px;line-height: 40px;background-color: #363C44;color: white;font-weight: 400;font-size: 11px;border-left: 2px;border-right: 2px;border-bottom: 2px;border-top: 2px;border-style: solid;">Mon compte</a></li>
+             <p style="display:inline;padding-right:10px;font-weight:300;">Bienvenue {{ Auth::user()->nom}} {{ Auth::user()->prenom}}
+             <a href="{{ url('/logout') }}" style="font-size: 9px;font-weight: 600;padding-left: 15px;">Se deconnecter</a></p>
+        @endif
+      </ul>
+    </div>
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
-                </a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-        </div>
-    </nav>
-
+  </nav>
     @yield('content')
-
     <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script>
+ $(document).ready(function() {
+    $('select').material_select();
+  });
+</script>
 </body>
 </html>
--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -1,17 +1,409 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container"">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
-
-                <div class="panel-body">
-                    Your Application's Landing Page.
+        <div class="col-md-10 col-md-offset-1" style="text-align:center;color:#363C44;font-weight:300;padding-top:50px;">
+                <h2 style="font-weight:300;">Nous proposons des lavages parfaits</h2>
+                <h3 style="font-weight:300;">Depuis 1998, 100% Qualité, 100% Eco</h3>
+                <img src="/img/appointmentbutton.png" class="animated infinite pulse" style="width:200px;padding-top:15px;">
+                <h5 style="font-weight:300;">DECOUVREZ LE PRESSING NOUVELLE GENERATION <br>ET NOS TECHNIQUES DE NETTOYAGE BREVETÉ</h3>
+                <img src="/img/arrow.png" class="animated infinite bounce" style="width:20px;padding-top:5px;">
                 </div>
             </div>
         </div>
     </div>
+</div>
+<div class="container" style="background-color:white;width:100%;background-image:url(img/bg2.png);background-size: cover;"">
+<div class="row">
+<div class="col s12 m4 l2"><p></p></div>
+<div class="col s12 m4 l8" id="infos" style="text-align:left;color:#363C44;font-weight:300;padding-top:50px;padding-bottom:50px;""> <h2 style="font-weight:300;">Qui sommes nous ? </h2>
+<h5 style="font-weight:300;">Qu'il s'agisse d'une chemise, d'un pantalon en cuir ou d'un tapis, notre equipe vous offre un service de nettoyage professionnel pour tous vos articles ainsi que des services supplémentaires, tels que l'enlèvement et la livraison de vos vêtements à domicile, un service de retouche, etc.﻿ Grâce à notre grande expérience, vos textiles les plus délicats n'ont rien à craindre. Avec nous, ils sont entre de bonnes mains.﻿<br><br>
+
+<h3>SAVOIR FAIRE</h3>
+
+<b style="background-color:#FF5722">Un pré-détachage minutieux</b>
+Tous les vêtements dont nous prenons soin sont d’abord inspectés. Ils sont toujours préparés avant le lavage, c’est ce que nous appelons le pré-détachage. Il est important de noter que l'efficacité du GreenEarth®rend le pré-détachage plus rapide et plus simple. Le vêtement est ainsi examiné, chaque tâche est identifiée puis traitée selon sa nature pour s’assurer qu’elle partira parfaitement au lavage. Pour chaque type de tâche, nous appliquons un produit de pré-détachage ciblé, grâce à une gamme de produits spécialement développée pour notre pressing.<br><br>
+
+<b style="background-color:#FF5722">Un lavage doux et efficace</b>
+Nous remplissons notre machine avec du Siloxane, un fluide incolore, inodore et respectueux de l'environnement, qui fait disparaitre les tâches en prenant soin des vêtements. Il assouplit les fibres, adoucit les matières et ravive les couleurs. Les vêtements flottent dans un bain de silicone. Les tâches sont délicatement enlevées. Les vêtements les plus fragiles sont bercés pendant une heure, sans agression, sans mouvements brusques.<br><br>
+
+<b style="background-color:#FF5722">Un repassage plus délicat pour les vêtements</b>
+Le procédé de lavage écologique que nous utilisons n’assèche pas les fibres, mais il les protège, les gaine et les renforce, un peu comme un shampoing 2 en 1 pour vos cheveux ! Ainsi, les fibres sont plus soyeuses, plus douces et sont donc plus faciles à repasser. Chez Pressing des Halles, nous repassons ainsi de manière plus délicate, à une température plus basse. Les fibres ne sont pas lustrées et le vêtement reprend naturellement sa forme d’origine.<br><br>
+</h3></div>
+    <div class="col s12 m4 l2"><p></p></div>
+        <div class="col-md-10 col-md-offset-1" style="text-align:left;color:#363C44;font-weight:300;padding-top:50px;padding-left:50px;padding-right:50px;">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- TARIFS -->
+<div class="container" style="width:100%;">
+<div class="row">
+<div class="col s12" id="tarifs" style="text-align:center;color:#363C44;font-weight:300;padding-top:50px;padding-bottom:50px;padding-left:20px;padding-right:20px;"">
+<h9 style="font-weight: 300;background-color: #363C44;color: white;padding-left: 5px;padding-right: 5px;padding-top: 5px;padding-bottom: 5px;font-size: 40px;font-family: 'Lato';">TARIFS </h9>
+<h5 style="font-weight:300;"></h3></div>
+</div>
+</div>
+</div>
+<div class="row">
+<h4 style="padding-left:15px;color:#363C44;">PRESSING VETEMENTS et BLANCHISSERIE</h4>
+<!-- TABLEAU TARIFS -->
+<div class="col s12 m6 l3">
+<!-- COL 1 Tarifs -->
+<table style="font-weight: 300;">
+    <thead style="color:#363C44;border-bottom:0px;">
+          <tr>
+              <th data-field="name"></th>
+              <th data-field="price"></th>
+          </tr>
+        </thead>
+        <tbody style="color:#363C44">
+          <tr>
+            <td>Veste</td>
+            <td>9,00€</td>
+          </tr>
+          <tr>
+            <td>Veste soft</td>
+            <td>11,00€</td>
+          </tr>
+          <tr>
+            <td>Pantalon</td>
+            <td>5,90€</td>
+          </tr>
+            <tr>
+            <td>Pantalon soft</td>
+            <td>7,10€</td>
+          </tr>
+            </tr>
+            <tr>
+            <td>Chemise </td>
+            <td>5,20€</td>
+          </tr>
+            <tr>
+            <td>Chemise soft </td>
+            <td>7,10€</td>
+          </tr>
+            <tr>
+            <td>Jupe courte </td>
+            <td>5,00€</td>
+          </tr>
+            <tr>
+            <td>Jupe longue </td>
+            <td>9,90€</td>
+          </tr>
+            <tr>
+            <td>Jupe courte soft</td>
+            <td>8,00€</td>
+          </tr>
+            <tr>
+            <td>Jupe longue soft</td>
+            <td>12,00€</td>
+          </tr>
+            <tr>
+            <td>Jupe plisée soleil</td>
+            <td>15,00€</td>
+          </tr>
+            <tr>
+            <td>Jupe plisée soleil</td>
+            <td>15,00€</td>
+          </tr>
+            <tr>
+            <td>Robe courte</td>
+            <td>8,90€</td>
+          </tr>
+            <tr>
+            <td>Robe longue</td>
+            <td>12,80€</td>
+          </tr>
+            <tr>
+            <td>Robe plissée soleil</td>
+            <td>18,00€</td>
+          </tr>
+            <tr>
+            <td>Robe courte soft</td>
+            <td>11,00€</td>
+          </tr>
+            <tr>
+            <td>Pull plié</td>
+            <td>6,20€</td>
+          </tr>
+            <tr>
+            <td>Tee shirt</td>
+            <td>4,50€</td>
+          </tr>
+            <tr>
+            <td>Pull soft court</td>
+            <td>6,90€</td>
+          </tr>
+            <tr>
+            <td>Pantalon soft long</td>
+            <td>7,30€</td>
+          </tr>
+            <tr>
+            <td>Etole</td>
+            <td>9,00€</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="col s12 m6 l3" style="border-right: 2px;border-top: 0px;border-bottom: 0px;border-style: solid;border-left: 0px;border-color: #363C44;"">
+    <!-- COL 2 Tarifs -->
+    <table style="font-weight: 300;">
+    <thead style="color:#363C44;border-bottom:0px;">
+          <tr>
+              <th data-field="name"></th>
+              <th data-field="price"></th>
+          </tr>
+        </thead>
+        <tbody style="color:#363C44">
+          <tr>
+            <td>Sweat plié</td>
+            <td>6,70€</td>
+          </tr>
+          <tr>
+            <td>Bustier</td>
+            <td>8,00€</td>
+          </tr>
+          <tr>
+            <td>Gilet</td>
+            <td>5,90€</td>
+          </tr>
+            <tr>
+            <td>Gilet soft</td>
+            <td>6,60€</td>
+          </tr>
+            </tr>
+            <tr>
+            <td>Bermuda </td>
+            <td>6,00€</td>
+          </tr>
+            <tr>
+            <td>Combinaison short </td>
+            <td>10,00€</td>
+          </tr>
+            <tr>
+            <td>Costume tailleur </td>
+            <td>15,00€</td>
+          </tr>
+            <tr>
+            <td>Costume tailleur soft </td>
+            <td>18,00€</td>
+          </tr>
+            <tr>
+            <td>Tailleur jupe </td>
+            <td>14,50€</td>
+          </tr>
+            <tr>
+            <td>Polo</td>
+            <td>4,90€</td>
+          </tr>
+            <tr>
+            <td>Cravate</td>
+            <td>3,80€</td>
+          </tr>
+            <tr>
+            <td>Cravate soft</td>
+            <td>4,90€</td>
+          </tr>
+            <tr>
+            <td>Manteau court</td>
+            <td>22,00€</td>
+          </tr>
+            <tr>
+            <td>Imperméable</td>
+            <td>15,00€</td>
+          </tr>
+            <tr>
+            <td>Conbinaison de ski</td>
+            <td>18,00€</td>
+          </tr>
+            <tr>
+            <td>Pantalon de ski</td>
+            <td>10,00€</td>
+          </tr>
+            <tr>
+            <td>Conbinaison de course</td>
+            <td>11,00€</td>
+          </tr>
+            <tr>
+            <td>Robe de mariée</td>
+            <td>120,00€</td>
+          </tr>
+            <tr>
+            <td>Robe de soirée</td>
+            <td>20,00€</td>
+          </tr>
+            <tr>
+            <td>Robe de soirée soft</td>
+            <td>30,00€</td>
+          </tr>
+            <tr>
+            <td>Foulard</td>
+            <td>3,90€</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="col s12 m6 l3">
+    <!-- COL 3 Tarifs -->
+    <table style="font-weight: 300;">
+    <thead style="color:#363C44;border-bottom:0px;">
+          <tr>
+              <th data-field="name"></th>
+              <th data-field="price"></th>
+          </tr>
+        </thead>
+        <tbody style="color:#363C44">
+          <tr>
+            <td>Drap plat</td>
+            <td>3,40€</td>
+          </tr>
+          <tr>
+            <td>Drap house</td>
+            <td>4,60€</td>
+          </tr>
+          <tr>
+            <td>Taie</td>
+            <td>1,50€</td>
+          </tr>
+            <tr>
+            <td>Drap plat ancien/lin/soft</td>
+            <td>5,50€</td>
+          </tr>
+            </tr>
+            <tr>
+            <td>Drap house ancien/lin/soft </td>
+            <td>6,00€</td>
+          </tr>
+            <tr>
+            <td>Taie ancien/lin/soft </td>
+            <td>2,00€</td>
+          </tr>
+            <tr>
+            <td>Protège matelas </td>
+            <td>6,50€</td>
+          </tr>
+            <tr>
+            <td>House de couette 1 pers </td>
+            <td>7,00€</td>
+          </tr>
+            <tr>
+            <td>House de couette 1 pers </td>
+            <td>9,50€</td>
+          </tr>
+            <tr>
+            <td>House de couette</td>
+            <td>15,00€</td>
+          </tr>
+            <tr>
+            <td>Serviette de toilette</td>
+            <td>1,80€</td>
+          </tr>
+            <tr>
+            <td>Serviette de toilette</td>
+            <td>3,50€</td>
+          </tr>
+            <tr>
+            <td>Draps de bain</td>
+            <td>4,90€</td>
+          </tr>
+            <tr>
+            <td>Peignoir éponge</td>
+            <td>4,60€</td>
+          </tr>
+            <tr>
+            <td>Drap de bain XL</td>
+            <td>5,90€</td>
+          </tr>
+            <tr>
+            <td>Torchon</td>
+            <td>1,50€</td>
+          </tr>
+
+        </tbody>
+      </table>
+    </div>
+    <div class="col s12 m6 l3">
+    <!-- COL 4 Tarifs -->
+    <table style="font-weight: 300;">
+    <thead style="color:#363C44;border-bottom:0px;">
+          <tr>
+              <th data-field="name"></th>
+              <th data-field="price"></th>
+          </tr>
+        </thead>
+        <tbody style="color:#363C44">
+          <tr>
+            <td>Couette enfant</td>
+            <td>16,00€</td>
+          </tr>
+          <tr>
+            <td>Couette 1 pers</td>
+            <td>20,00€</td>
+          </tr>
+          <tr>
+            <td>Couette 2 pers</td>
+            <td>25,00€</td>
+          </tr>
+            <tr>
+            <td>Couette soie</td>
+            <td>35,50€</td>
+          </tr>
+            </tr>
+            <tr>
+            <td>House de clic clac </td>
+            <td>29,00€</td>
+          </tr>
+            <tr>
+            <td>Couette duvet 2 pers </td>
+            <td>35,00€</td>
+          </tr>
+            <tr>
+            <td>Dessus de lit a sec </td>
+            <td>30,50€</td>
+          </tr>
+            <tr>
+            <td>Dessus de lit lavage </td>
+            <td>22,00€</td>
+          </tr>
+            <tr>
+            <td>Couverture a sec </td>
+            <td>27,00€</td>
+          </tr>
+            <tr>
+            <td>Couverture en lavage</td>
+            <td>23,00€</td>
+          </tr>
+            <tr>
+            <td>Rideaux voilage au m2</td>
+            <td>3,00€</td>
+          </tr>
+            <tr>
+            <td>Rideaux au m2</td>
+            <td>4,00€</td>
+          </tr>
+            <tr>
+            <td>Rideaux doublé/lin au m2</td>
+            <td>5,90€</td>
+          </tr>
+            <tr>
+            <td>Serviette de table</td>
+            <td>1,80€</td>
+          </tr>
+            <tr>
+            <td>Nappe brodée courte</td>
+            <td>8,90€</td>
+          </tr>
+            <tr>
+            <td>Nappe longue</td>
+            <td>12,50€</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </div>
 @endsection
